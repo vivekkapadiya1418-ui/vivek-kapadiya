88d05a8cca58621eafaa41681ddb2001c289f566
vivekkapadiya1418-ui <vivekkapadiya1418@gmail.com>
2026-01-07 09:17:51 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,33 @@
+x=10
+print(x)
+
+a=5
+print(-a)
+
+p=10
+q=5
+print(p>q)
+print(p==p)
+
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+is_pass=True
+print(is_pass)
+
+a=5
+b=3
+print(a & b)
+print(a | b)
+
+number=[1,2,3,4,5,6]
+print(2 in numbers)
+print(5 not in numbers)
+
+x=10
+y=10
+print(x is y)
+print(x is not y)
