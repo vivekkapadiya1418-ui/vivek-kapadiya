1dd3191b15f60da15bb6d9f774cd5a71abd08542
vivekkapadiya1418-ui <vivekkapadiya1418@gmail.com>
2026-02-02 10:23:14 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,26 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #int
+>>> print("20")
+20
+>>> print("22")
+22
+>>> #flot
+>>> print("88.88")
+88.88
+>>> print("55.22")
+55.22
+>>> #bool
+>>> #str
+>>> print("hello rajcot")
+hello rajcot
+>>> print("hiiii")
+hiiii
+>>> #bool
+>>> A=True
+>>> B=False
+>>> print(A)
+True
+>>> print(B)
+False
+>>> 
