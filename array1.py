c9bd4e56bdda51e34950dda509d1dcb7a781103b
vivekkapadiya1418-ui <vivekkapadiya1418@gmail.com>
2026-03-09 10:22:49 +0530

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,39 @@
+ """#1.len()
+import array
+arr = array.array('i',[10,20,30,40,50])
+print(len(arr))"""
+
+"""#2.append()
+import array
+arr = array.array('i',[10,20,30])
+arr.append(40)
+print(arr)"""
+
+"""#3.insert()
+import array
+arr = array.array('i',[10,20,40])
+arr.insert(2,30)
+print(arr)"""
+
+"""#4.remove
+import array
+arr = array.array('i',[10,20,30,20,40])
+arr.remove(20)
+print(arr)"""
+
+"""#5.pop()
+import array
+arr = array.array('i',[10,20,30,40])
+x = arr.pop()
+print("Removed:",x)
+print(arr)"""
+
+"""#6.index()
+import array
+arr = array.array('i',[10,20,30,40])
+print(arr.index(30))"""
+
+#7.count()
+import array
+arr = array.array('i',[10,20,30,20,40])
+print(arr.count(20))
