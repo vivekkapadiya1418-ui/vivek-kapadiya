523038ba4ea0d1a2d27f5c81de98cab7ceb017e8
vivekkapadiya1418-ui <vivekkapadiya1418@gmail.com>
2026-03-09 10:40:24 +0530

    Add files via upload

---
+++ array2.py
@@ -0,0 +1,31 @@
+"""# positive indexing
+import array
+
+arr = array('i',[10,20,30,40,50])
+
+print(arr[0])   # first element
+print(arr[2])   # third element
+print(arr[4])   # fifth element"""
+
+"""# negative indexing
+import array
+
+arr = array('i',[10,20,30,40,50])
+
+print(arr[-1])   # last element
+print(arr[-2])   # second last element
+print(arr[-5])   # first element"""
+
+"""# modifing element 
+import array
+
+arr = array('i',[10,20,30,40,50])
+
+arr[2] = 35
+
+print(arr)"""
+
+#slieng in array
+arr = array.array('i',[10,20,30])
+
+print(arr[5])
