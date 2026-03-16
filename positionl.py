0f2e01d4b668e289ae8e2f02a0058ec1a7a719cb
vivekkapadiya1418-ui <vivekkapadiya1418@gmail.com>
2026-03-16 10:33:06 +0530

    Add files via upload

---
+++ positionl.py
@@ -0,0 +1,63 @@
+"""#1 basic possition
+def add(a, b):
+    print("a =", a)
+    print("b =", b)
+    return a + b
+
+result = add(2, 5)
+print("Sum:", result"""
+
+"""#2student info
+def student_info(name, roll, marks):
+    print("Name:", name)
+    print("Roll No:", roll)
+    print("Marks:", marks)
+
+student_info("Ravi", 101, 85)"""
+
+"""#3 simple intrest
+def simple_interest(p, r, n):
+    si = (p * r * n) / 100
+    print("Simple Interest:", si)
+
+simple_interest(10000, 2, 2)
+simple_interest(50000, 1.2, 3)"""
+
+"""#4 area of circle
+def area_circle(r):
+    area = 3.14 * r * r
+    print("Area of circle:", area)
+
+area_circle(7.5)
+area_circle(4)"""
+
+"""5 check no positive negative or zero
+def check_value(no):
+    if no > 0:
+        print("Positive")
+    elif no < 0:
+        print("Negative")
+    else:
+        print("Zero")
+
+check_value(0)
+check_value(90)
+check_value(-15)"""
+
+"""#6 odd or even
+def odd_even(no):
+    if no % 2 == 0:
+        print(f"Value {no} is Even")
+    else:
+        print(f"Value {no} is Odd")
+
+odd_even(50)
+odd_even(15)"""
+
+#7 multiplication and divison
+def addition(a, b):
+    add = a + b
+    print("Addition of two values:", add)
+
+addition(50, 10.5)
+addition(100, 200)
